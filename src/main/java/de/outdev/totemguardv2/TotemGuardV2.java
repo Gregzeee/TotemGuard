f920e4db37ae351cea69925df943c8e18365b3e1
CordasMC <137982087+CordasMC@users.noreply.github.com>
2024-08-11 15:23:41 +0200

    removes useless stuff

--- src/main/java/de/outdev/totemguardv2/TotemGuardV2.java
+++ src/main/java/de/outdev/totemguardv2/TotemGuardV2.java
@@ -19,16 +19,9 @@ public final class TotemGuardV2 extends JavaPlugin {
         return instance;
     }
 
-    @Nullable
-    public static FileConfiguration getConfiguration() {
-        return config;
-    }
-
     @Override
     public void onEnable() {
         getLogger().info("Loading TotemGuard...");
-
-        long start = System.currentTimeMillis();
 
         getCommand("totemguard").setExecutor(new TotemGuardCommand());
         getCommand("check").setExecutor(new CheckCommand());
@@ -38,7 +31,7 @@ public final class TotemGuardV2 extends JavaPlugin {
 
         saveDefaultConfig();
 
-        totemUseListener = new TotemUseListener(this);
+        getServer().getPluginManager().registerEvents(totemUseListener = new TotemUseListener(), this);
 
         getLogger().info(" \n"+
                 "  _____    _             ___                  _ \n" +
@@ -46,13 +39,10 @@ public final class TotemGuardV2 extends JavaPlugin {
                 "   | |/ _ \\  _/ -_) '  \\ (_ | || / _` | '_/ _` |\n" +
                 "   |_|\\___/\\__\\___|_|_|_\\___|\\_,_\\__,_|_| \\__,_|\n" +
                 "                                                ");
-        long finish = System.currentTimeMillis() - start;
-        getLogger().info("Successfully loaded TotemGuard in " + finish + "ms!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Disabling plugin 'TotemGuard'...");
         saveDefaultConfig();
     }
 
